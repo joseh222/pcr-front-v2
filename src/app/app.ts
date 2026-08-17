5c903fe184526911573dd4dcf14e8623148fb5ea
joseh222 <134876438+joseh222@users.noreply.github.com>
2026-08-17 15:26:14 -0500

    chore: configure angular material foundation (#1)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'pcr-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('PCR_FrontV2');
+  protected readonly title = 'PCR Front V2';
 }
